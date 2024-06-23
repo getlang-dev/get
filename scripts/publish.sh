c71e9bfa0293f71e8e69fcabd4c1e023a75bd204
Matt Fysh <mattfysh@gmail.com>
2024-06-23 14:37:20 +1000

    bump

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,10 +9,12 @@ tsc -p packages
 for dir in dist/*; do
   pkg=$(basename "$dir")
   mv "$dir" "packages/$pkg/dist"
+  sed -i 's/workspace://g' "packages/$pkg/package.json"
 done
 
 changeset publish
 
 rm -rf dist packages/*/dist
+git restore packages/*/package.json
 
 git push --follow-tags
