e656527b0133cb7c8411f99d2d3de0559d2e8795
Matt Fysh <mattfysh@gmail.com>
2024-12-08 12:01:20 +1000

    fix publish

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,10 +5,10 @@ set -e
 for dir in packages/*; do
   tsc -p $dir
   sed -i.bak 's/workspace://g' "$dir/package.json"
-  rm "$dir/package.json.bak"
+  rm "$dir/package.json.bak" "tsconfig.json"
 done
 
 changeset publish
 
 rm -rf dist packages/*/dist
-git restore packages/*/package.json
+git restore packages
